ed90a0ae9169e516f412a96713a09c8513439ce1
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-18 10:56:16 +0300

    js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const name = "Artem";
+
+//1)ключове слово
+// 2) Назва вашої змінної
+// 3) = оператор присвоєння
+// 4) значення
+
+const hobby = "Грати";
+console.log(hobby);
+//перше слово з маленької, всі інші починаються з великої
+const veryLongNameHere = "";
+
+// const - яку не можна змінювати або перевизначати в майбутньому
+// let - не обовʼязково давати значення одразу
+// ви мижете змінювати його нижче у коді безліч разів
+
+let age;
+console.log(age);
+
+// age = 20;
+
+// age = 200;
+
+const string = "afnasdonfkasndkfnaowenfoawneofnownfownf";
+console.log(typeof string);
+const number = 40;
+console.log(typeof number);
+// true = - правда
+// false - не правда
+const boolean = true || false;
+console.log(boolean);
+console.log(typeof boolean);
+
+// конкатиннація - зшивання строк
+console.log(number);
+
+alert("Все пропало, все погано, все упало");
+
+const message = confirm("Чи згодні ви?");
+console.log(message);
+
+const asdas = prompt("Введи свій нік");
+console.log(asdas);

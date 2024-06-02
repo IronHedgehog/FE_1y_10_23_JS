134000c3d13909c04f1805bb36175390f3ba9c79
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-02 12:03:10 +0300

    DOM

--- js/index.js
+++ js/index.js
@@ -1,88 +1,74 @@
-const name = "                   Artem is here                      ";
-const nameLength = name.trim().length;
+// DOM - Дозволяє отримати доступ до HTML тегів та атрибутів та їх замінювати або проводити якісь інші дії
 
-// .trim() - метод для обрізання пробілів перед буквами та після букв (метод)
-//
-//length - довжина (властивість)
-console.log(name);
-console.log(nameLength);
+// document - це наша HTML сторінка
 
-const string = "shoc";
-// s = 0
-// h = 1
-//0 = 2
-// c = 3
+const navButton = document.querySelector(".nav-button");
+// достатньо популярні
+// document.getElementById();
+// отримує HTML елемент за його ID
+// document.getElementsByClassName();
+// отримує HTML елементи за його class
+// Популярні
+// document.querySelector();
+// Дозволяє вам отримати доступ до HTML тегу за будь-якою ознакою
+// .querySelector() - якщо багато співпадінь, знайде тільки перший збіг
 
-const stringLength = string.length; // 4
+// ознака - id,class,атрибут, тег
+// document.querySelectorAll();
+// querySelectorAll(); - знаходить всі елементи з однаковою ознакою
 
-const string2 = name + string;
+console.log(navButton);
 
-console.log(string2.length - 1);
+// textContent - звертається до текстового наповнення HTML елементу
+navButton.textContent = "ТОвару немає";
 
-const string3 = "ABC" + name + "CBA" + " " + "BAC"; // Конкатенація
-const string4 = `ABC CBA, ${name}  ,BAC`; // інтерполяція
+// Щоб отримати доступ до будь-якого HMTL елемента ми маємо використати один з методів наведених вище (зазвичай querySelector())
+const input = document.querySelector(".input");
+console.log(input.value);
+//.value - це html атрибут тегу input який зберігає значення input
+//  <input class="checkbox" type="checkbox" />
+const checkbox = document.querySelector(".checkbox");
+checkbox.checked = true;
 
-console.log(string3);
-console.log(string4);
+const img = document.getElementById("img");
+img.src = "../img/flower.jpg";
 
-console.log(string2);
+// const navBar = document.querySelector(".header-nav");
 
-const string5 = "SaLe";
+// document.body - до тегу body можна звернутись напряму через документ
+// JS
 
-const postCard = "sAlE";
-if (postCard.toLowerCase() === "sale") {
-  console.log(postCard.toLowerCase());
-  console.log(postCard.toUpperCase());
-  console.log("Піймали та видалили спам");
-}
+document.body.style.backgroundColor = "darkblue";
+// CSS
+// body {
+// backgroundColor: darkblue
+// }
 
-const string6 = "s i x";
-console.log(string6.indexOf(" ")); // -1
-console.log(string6.indexOf("S")); //чутливий до регістру
-console.log(string6.indexOf("x")); // 0
-// indexOf() - повертає індекс символа у рядку
-// -1 - index то індекс який повертається якщо нічого не знайдено
-// indexOF()- оперує тільки числами
-// indexOf - чутливий до регістру
-// Знаходить тільки перше співпадіння
+const dark = document.querySelector(".dark");
+const white = document.querySelector(".white");
 
-const string7 = "String";
-// Чи включає змінна string7 у себе ВЕЛИКУ літеру S?
-console.log(string7.includes("string"));
+// dark.addEventListener("click", darkTheme);
+// white.addEventListener("click", whiteTheme);
 
-// includes() - повертає true або false
-// чутливий до регістру
+// function darkTheme(e) {
+//   document.body.style.backgroundColor = "darkblue";
+// }
 
-const string8 = "something.txt";
+// function whiteTheme(e) {
+//   document.body.style.backgroundColor = "white";
+// }
 
-// чи починається рядок в змінній string8 з вказаних символів?
-console.log(string8.startsWith("some"));
-// чи закінчується рядок в змінній string8 з вказаних символів?
-console.log(string8.endsWith(".txt"));
-// повертає true або false
+// classList - список класів
+// add - додати
+white.classList.add("something");
+// remove - видалити
+white.classList.remove("something");
+console.log(white.classList);
+// toggle - працює в обидві сторони може додати та видалити
+white.classList.toggle("something");
+// contains - чи включає класліст такий клас?
+white.classList.contains("asasdasdasd");
+console.log(white.classList.contains("somasdasdething"));
 
-const string9 =
-  "                A                                        R          ";
-// trim() - видаляє пробіли на початку та вкінці рядка
-console.log(string9.trim());
-
-const string10 = "String10";
-
-console.log(string10.padStart(2, "A"));
-// 2 56 37
-const hours = "5";
-
-console.log(hours.padStart(2, 0));
-
-// padStart (2)
-//padStart - перший параметр вказує на те скільки символів має бути у рядку
-// Якщо така кількість символів вже є то padStart поверне вам рядок з яким працює
-// Додає всі символи на початок рядка
-// padEnd;() - додає символи в кінець рядка
-console.log(string10.padEnd(20, "r"));
-
-console.log(`${string10}`);
-
-const user = "Yura";
-
-console.log(`Привіт, ${user}`); // інтерполяція
+// events 5
+// if else switch case 6

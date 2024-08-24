1f61da2e091e48aeb3a90f86308c6dc73d382d04
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-08-24 12:29:49 +0300

    a

--- js/Dz.js
+++ js/Dz.js
@@ -1,148 +1,35 @@
-/*
- * Типів транзацкій всього два.
- * Можна покласти або зняти гроші з рахунку.
- */
-
-// Напиши сценарій керування особистим кабінетом інтернет-банку. Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
-const Transaction = {
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw",
+// НЕ ПРАЦЮЄ
+const user = {
+  name: "Mango",
+  age: 20,
+  // hobby: "html",
+  // premium: true,
 };
 
-let randomId = Math.floor(Math.random() * 1000 + 1);
-/*
- * Кожна транзакція - це об'єкт з властивостями: id, type і amount
- */
+// premium: newPremium - означає, що ви переіменвали змінну
 
-const account = {
-  // Поточний баланс рахунку
-  balance: 0,
+//  = встановлення значення за замовчуванням ( це є значення за замовчуванням - коли такого поля не існує в обʼєкті який ви деструктуризуєте)
 
-  // Історія транзакцій
-  transactions: [],
+const {
+  name,
+  age,
+  hobby: newHobby = (user.hobby = "shoc"),
+  premium: newPremium = false,
+  mood = "happy",
+} = user;
 
-  /*
-   * Метод створює і повертає об'єкт транзакції.
-   * Приймає суму і тип транзакції.
-   * Кожна транзакція - це об'єкт з властивостями: id, type і amount
-   */
-  createTransaction(amount, type) {
-    // id - має бути унікальним
-    // Метод створює
-    const transaction = {
-      amount,
-      type,
-      id: randomId,
-      // id: 1,
-    };
+console.log(name, age, newHobby, newPremium, mood);
 
-    return transaction;
-  },
+// const { name: name2, age: age2 } = { name: "Mango", age: 2 };
 
-  /*
-   * Метод відповідає за додавання суми до балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій
-   */
-  deposit(amount) {
-    if (amount <= 0) {
-      console.log("Додати 0 або менше 0 не можна");
-      return;
-    }
+const anyUser = { name1: "Mango", age1: 2 };
 
-    const depositTransaction = this.createTransaction(
-      amount,
-      Transaction.DEPOSIT
-    );
+const { name1, age1: age2 } = anyUser;
 
-    //  Метод відповідає за додавання суми до балансу.
-    this.balance += amount;
-    // Викликає createTransaction для створення об'єкта транзакції
+console.log(name1, age2);
 
-    // push - він дозволяє додати що передасте в масив
-    //  після чого додає його в історію транзакцій
-    this.transactions.push(depositTransaction);
-  },
+const numbers = [1, 2, 3, 4, 5];
 
-  /*
-   * Метод відповідає за зняття суми з балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій.
-   *
-   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
-   * про те, що зняття такої суми не можливо, недостатньо коштів.
-   */
-  withdraw(amount) {
-    // Метод відповідає за зняття суми з балансу.
-    if (amount > this.balance) {
-      console.log(
-        `зняття такої суми не можливо, недостатньо коштів. Вам не вистачає ${
-          amount - this.balance
-        } грн`
-      );
-      return;
-    }
-    const withdrowTransaction = this.createTransaction(
-      amount,
-      Transaction.WITHDRAW
-    );
+const [one, two, three, ...rest] = numbers;
 
-    this.balance -= amount;
-    //  Викликає createTransaction для створення об'єкта транзакції
-
-    // після чого додає його в історію транзакцій.
-    this.transactions.push(withdrowTransaction);
-  },
-
-  /*
-   * Метод повертає поточний баланс
-   */
-  getBalance() {
-    return this.balance;
-  },
-
-  /*
-   * Метод шукає і повертає об'єкт транзакції по id
-   */
-  getTransactionDetails(id) {
-    for (const transaction of this.transactions) {
-      // Метод шукає
-      if (id === transaction.id) {
-        // повертає об'єкт транзакції
-        return transaction;
-      }
-    }
-  },
-
-  /*
-   * Метод повертає кількість коштів
-   * певного типу транзакції з усієї історії транзакцій
-   */
-  getTransactionTotal(type) {
-    let result = 0;
-    for (const transaction of this.transactions) {
-      if (transaction.type === type) {
-        result += transaction.amount;
-      }
-    }
-    return result;
-  },
-};
-
-console.log(account.createTransaction(1000, Transaction.DEPOSIT));
-// console.log(account.createTransaction(1000, "Deposit"));
-
-account.deposit(1000);
-account.withdraw(1000);
-
-// console.log(account.getBalance());
-console.log(account.getTransactionDetails(randomId));
-account.deposit(1000);
-
-// console.log(account.getBalance());
-
-console.log(account.getTransactionTotal(Transaction.DEPOSIT));
-console.log(account.getTransactionTotal(Transaction.WITHDRAW));
-console.log(account.getBalance());
+console.log(one, two, three, rest);
--- js/index.js
+++ js/index.js
@@ -1,117 +1,20 @@
-// // const numbers = [20, 10, 60, 70, 40];
+// Імперативний код - код в якому ми описали кожен наш крок
+const numbers = [1, 23, 4, 5, 6];
 
-// // //min() - очікує набір окремих чисел (1,2,3,4,5,6)
-// // //min() - [] - масив -це не число
-// // console.log(Math.min(numbers)); // NaN
+let counter = 0;
 
-// // console.log(numbers);
-// // // spread - розділення
-// // console.log(...numbers);
-// // console.log(Math.min(...numbers));
-// // // arguments (arguments)
-// // // ...rest (Збирає масиви)
-// // const func = (a, b, c, ...numbers) => {
-// //   console.log(...numbers);
-// // };
+for (let i = 0; i < numbers.length; i++) {
+  counter += numbers[i];
+}
 
-// // func(20, 102, 30, 405, 20, 10, 20, 30, 405);
+console.log(counter);
 
-// // const legoCollection = ["newLego", ...numbers, "newLego"];
+//Декларативний код  - основна реалізація коду прихована
 
-// // const obj = {
-// //   a: "a",
-// //   b: "b",
-// // };
+const number1 = [1, 23, 4, 5, 6];
+let counter1 = 0;
 
-// // const newObjects = {
-// //   c: "c",
-// //   ...obj,
-// //   d: "d",
-// // };
-
-// // console.log(newObjects === obj);
-// // console.log(legoCollection);
-
-// const a = {
-//   name: "Petro",
-// };
-
-// const zyctrichSybota = {};
-
-// const zyctrcih1Sybota = zyctrichSybota;
-
-// console.log(zyctrichSybota === zyctrcih1Sybota);
-
-// const zyctrichNedilia = {};
-
-// const zyctrich1Nedilia = zyctrichNedilia;
-
-// console.log(zyctrichNedilia === zyctrich1Nedilia);
-
-// // Ці зустрічі вони мають різне посилання
-// console.log(zyctrichSybota === zyctrichNedilia);
-
-// const b = { ...a };
-
-// console.log(a);
-// console.log(b);
-
-// // Стандартна поведінка складних типів данних
-// console.log(a === b);
-// console.log(a == b);
-
-// // number - зберігається значення у памʼяті бо це примітив
-// const c = 5;
-
-// // objects - складний тип даних до якого відносяться масиви обʼєкти
-
-const obj = {
-  name: "Petro",
-  lastName: "Trendelis",
-  age: 30,
-  proGamer: false,
-  // Метод обʼєкта
-  goWalk() {
-    console.log("Піти гуляти");
-  },
-};
-
-//
-const namee = obj.name;
-const lastNamee = obj.lastName;
-const proGamerr = obj.proGamer;
-const agee = obj.age;
-
-console.log(namee);
-//Деструктуризація
-// Ключове слово
-// {} - літерал обʼєкта (Позначають обʼєкт)
-//{ назва поля значення якого ми хочемо отримати у швидкий доступ }
-// = - привласнення
-//obj - з якого обʼєкту ми бажаємо привласнити значення цього поля
-const {
-  name: userName,
-  lastName,
-  age,
-  proGamer,
-  goWalk,
-  terms = "false",
-} = obj;
-
-const { name: nameeee, ...rest } = obj;
-
-console.log(rest);
-
-//  name: userName, - : назву нової змінної
-// terms = "false" - значення за замовчуванням
-console.log(userName, terms);
-
-goWalk();
-
-//
-const nameee = userName;
-const lastNameee = lastName;
-const proGamerrr = proGamer;
-const ageee = age;
-
-// Привіт, ваш нікнейм
+number1.forEach((number, index, array) => console.log((counter1 += number)));
+// number1.forEach(function name(number, index, array) {
+//   console.log((counter1 += number));
+// });

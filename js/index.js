866d828cce9b690281da8a451fdeedaa37bdc642
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-09-07 12:24:47 +0300

    a

--- js/index.js
+++ js/index.js
@@ -261,3 +261,200 @@ console.log(sortedITDepartment);
 
 //Метод sort - функція з домішками(мотує початковий масив)
 //  тож робимо перед сортуванням копії(map(), slice(),[...ITDep].sort())
+
+const people = [
+  { name: "Artem", age: 23 },
+  { name: "Petro", age: 30 },
+  { name: "Arnold", age: 14 },
+];
+// reduce - калькулятор
+//reduce - може замінити усі перебираючі методи масиви
+const ageSum = people.reduce((acc, person) => acc + person.age, 0);
+console.log(ageSum);
+// reduce в данному повератє нам обʼєкт
+//першим параметром reduce отримує callback функцію яка отримує параметрами акумулятор(acc) та кожен елемент масиву по черзі(people)
+const newObject = people.reduce((acc, people) => {
+  //acc - acc - пустий обʼєкт який ми будемо заповнювати
+  // [people.name] - people - обʼєкт через крапку звертаємось до значення поля name обʼєкту
+  // = people.age; - ми присвоюємо до ключа значення віку кожного з людей
+  acc[people.name] = people.age;
+  // повертаємо заряджений акумулятор у зовнішній код
+  return acc;
+  // {} - початкове значення акумулятору
+}, {});
+
+console.log(newObject);
+
+// console.log();
+
+const doubleNum = numbers4.reduce((acc, number) => {
+  acc.push(number * 2);
+  return acc;
+}, []);
+
+console.log(doubleNum);
+
+const fruits = [
+  {
+    name: "Apple",
+    color: "red",
+  },
+  {
+    name: "banana",
+    color: "yellow",
+  },
+  {
+    name: "pear",
+    color: "green",
+  },
+  {
+    name: "cherry",
+    color: "red",
+  },
+];
+
+//Нам потрібно стоврити обʼєкт в якому у нас будуть у вигляді ключів кольори а у вигляді значень масив з фруктами
+const groupedByColor = fruits.reduce((acc, fruit) => {
+  // перевірили якщо ключа нема додаємо єму значення пустого масиву
+  // acc[fruit.color] - !acc[fruit.color] - якщо такого ключа немає
+  // acc[fruit.color] = []; - всім новим елементам за замовчуванням в значення запишеться путсий масив
+  if (!acc[fruit.color]) {
+    acc[fruit.color] = [];
+  }
+  // в кожен масив запушили імʼя фрукту
+  acc[fruit.color].push(fruit.name);
+  // acc[fruit.color] = fruit.name;
+  // Повернули результат у зовнішній код
+  return acc;
+}, {});
+
+console.log(groupedByColor);
+
+// Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
+
+const userss = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+
+const getNamesSortedByFriendsCount = (users) => {
+  // ланцюжок методів
+  //[...users] - робимо копію(Sort - мотує початковий масив данних)
+  //sortуемо за довжиною масиву друзів
+  // за допомогою map виводимо імена
+  return [...users]
+    .sort((a, b) => b.friends.length - a.friends.length)
+    .map((user) => user.name);
+};
+// console.log(copyUsers);
+
+userss;
+
+// const names = copyUsers.map((user) => user.name); .map((user) => user.name);
+// return copyUsers;
+
+console.log(getNamesSortedByFriendsCount(userss));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// Отримати масив всіх умінь всіх користувачів (поле skills), при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
+
+const getSortedUniqueSkills = (users) => {
+  // твій код
+  // .flatMap()
+
+  const allSkills = users.reduce((acc, user) => {
+    user.skills.forEach((skill) => {
+      if (!acc.includes(skill)) {
+        acc.push(skill);
+      }
+    });
+    return acc;
+  }, []);
+
+  return allSkills.sort();
+};
+
+console.log(getSortedUniqueSkills(userss));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]

34ee2407d9471c6f38262af115f99b22f4b1a635
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-09-22 11:19:44 +0300

    a

--- js/Dz.js
+++ js/Dz.js
@@ -209,3 +209,191 @@ console.log(builder.string); // '^.^'
 
 builder.pad("=");
 console.log(builder.string); // '=^.^='
+
+// class Car {
+//   /*
+//    * Додай статичний метод `getSpecs(car)`,
+//    * який приймає об'єкт-машину як параметр і виводить
+//    * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
+//    */
+//   static getSpecs(car) {
+//     console.log(
+//       `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`
+//     );
+//   }
+//   /*
+//    * Конструктор отримує об'єкт налаштувань.
+//    *
+//    * Додай властивості майбутнього екземпляра класу:
+//    *  speed - поточна швидкість, початкова 0
+//    *  price - ціна автомобіля
+//    *  maxSpeed - максимальна швидкість
+//    *  isOn - заведений автомобіль, значення true або false. Спочатку false
+//    *  distance - загальний кілометраж, спочатку 0
+//    */
+//   constructor({ price, maxSpeed }) {
+//     this.speed = 0;
+//     this._price = price;
+//     this.maxSpeed = maxSpeed;
+//     this.isOn = false;
+//     this.distance = 0;
+//   }
+
+//   /*
+//    * Додай геттер і сеттер для властивості price,
+//    * який буде працювати з властивістю ціни автомобіля.
+//    */
+
+//   /*
+//    * Додай код для того, щоб завести автомобіль
+//    * Записує у властивість isOn значення true
+//    */
+
+//   get price() {
+//     return this._price;
+//   }
+
+//   set price(newPrice) {
+//     return (this._price = newPrice);
+//   }
+
+//   turnOn() {
+//     this.isOn = true;
+//   }
+
+//   /*
+//    * Додай код для того, щоб заглушити автомобіль
+//    * Записує у властивість isOn значення false,
+//    * і скидає поточну швидкість в 0
+//    */
+//   turnOff() {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+//   /*
+//    * Додає до властивості speed отримане значення,
+//    * за умови, що результуюча швидкість
+//    * не більше, ніж значення властивості maxSpeed
+//    */
+
+//   accelerate(value) {
+//     if (this.speed + value <= this.maxSpeed) {
+//       this.speed += value;
+//     }
+//   }
+
+//   /*
+//    * Забирає від властивості speed отримане значення,
+//    * за умови, що результуюча швидкість не менше нуля
+//    */
+//   decelerate(value) {
+//     if (this.speed - value >= 0) {
+//       this.speed -= value;
+//     }
+//   }
+
+//   /*
+//    * Додає в поле distance кілометраж (hours * speed),
+//    * але тільки в тому випадку, якщо машина заведена!
+//    */
+
+//   drive(hours) {
+//     if (this.isOn) {
+//       this.distance += hours * this.speed;
+//     }
+//   }
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
+
+// static methods
+
+class Mathh {
+  static add(x, y) {
+    return x + y;
+  }
+
+  static multiply(x, y) {
+    return x * y;
+  }
+}
+
+console.log(Mathh.add(1, 2));
+
+// const dubl = new Mathh();
+// console.log(dubl.add);
+
+// Uncaught RangeError: Maximum call stack size
+
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+    );
+  }
+  constructor({ maxSpeed, price }) {
+    this.speed = 0;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = false;
+    this.distance = 0;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed = this.speed + value;
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed = this.speed - value;
+    }
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000

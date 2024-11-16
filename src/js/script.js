c85fbd6cc8819da55ad17beaf0bf5af2ae9d4d33
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-11-16 12:26:49 +0200

    a

--- src/js/script.js
+++ src/js/script.js
@@ -1,94 +1,28 @@
-const scientists = [
-  {
-    name: 'Albert',
-    surname: 'Einstein',
-    born: 1879,
-    dead: 1955,
-    id: 1,
-  },
-  {
-    name: 'Isaac',
-    surname: 'Newton',
-    born: 1643,
-    dead: 1727,
-    id: 2,
-  },
-  {
-    name: 'Galileo',
-    surname: 'Galilei',
-    born: 1564,
-    dead: 1642,
-    id: 3,
-  },
-  {
-    name: 'Marie',
-    surname: 'Curie',
-    born: 1867,
-    dead: 1934,
-    id: 4,
-  },
-  {
-    name: 'Johannes',
-    surname: 'Kepler',
-    born: 1571,
-    dead: 1630,
-    id: 5,
-  },
-  {
-    name: 'Nicolaus',
-    surname: 'Copernicus',
-    born: 1473,
-    dead: 1543,
-    id: 6,
-  },
-  {
-    name: 'Max',
-    surname: 'Planck',
-    born: 1858,
-    dead: 1947,
-    id: 7,
-  },
-  {
-    name: 'Katherine',
-    surname: 'Blodgett',
-    born: 1898,
-    dead: 1979,
-    id: 8,
-  },
-  {
-    name: 'Ada',
-    surname: 'Lovelace',
-    born: 1815,
-    dead: 1852,
-    id: 9,
-  },
-  {
-    name: 'Sarah E.',
-    surname: 'Goode',
-    born: 1855,
-    dead: 1905,
-    id: 10,
-  },
-  {
-    name: 'Lise',
-    surname: 'Meitner',
-    born: 1878,
-    dead: 1968,
-    id: 11,
-  },
-  {
-    name: 'Hanna',
-    surname: 'Hammarström',
-    born: 1829,
-    dead: 1909,
-    id: 12,
-  },
-];
+const div1 = document.querySelector('.div1');
+const div2 = document.querySelector('.div2');
+const div3 = document.querySelector('.div3');
+const ul = document.querySelector('.ul');
 
-const button = document.getElementById('first-button');
+div1.addEventListener('click', onClick);
+div2.addEventListener('click', onClick2);
+div3.addEventListener('click', onClick3);
 
-function scientist19th() {
-  scientists.forEach(scientist => console.log(scientist.name));
+ul.addEventListener('click', onClickLi);
+
+function onClick(e) {
+  console.log(e.currentTarget);
 }
 
-button.addEventListener('click', scientist19th);
+function onClick2(e) {
+  // e.stopPropagation();
+  console.log(e.currentTarget);
+}
+
+function onClick3(e) {
+  console.log(e.currentTarget);
+}
+
+function onClickLi(e) {
+  console.log(e.currentTarget);
+  console.log(e.target.textContent);
+}

efb4a6208d2280a3e0f475c88ffde2cffb6bd250
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-12-14 12:32:15 +0200

    a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("asd");
+import { customAlphabet } from "nanoid";
+
+const model = {};
+const nanoid = customAlphabet("Украфівіячс1234?-_&&", 20);
+model.id = nanoid();
+
+console.log(model);
+
+

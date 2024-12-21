dd6a3565605555eae2853ae931887d3f7afa88af
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-12-21 12:35:50 +0200

    a

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const hello = require("./js.js");
+
+import { hello } from './js/js';
+
+// console.log(hello);
+document.body.innerHTML = hello('Artem');
---
+++ src/js/js.js
@@ -0,0 +1,12 @@
+export function hello(name) {
+  return `Hello,${name}`;
+}
+
+export function goodbye(name) {
+  return `GoodBye,${name}`;
+}
+
+// module.exports = {
+//   hello,
+//   goodbye,
+// };

37f8fea8ba9c860b50bf234e9642000bfc39764e
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-12-28 12:36:45 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-// const hello = require("./js.js");
+const template = document.getElementById('template');
 
-import Car from './js/class';
-import { goodbye, hello } from './js/js';
-// console.log(hello);
+document.addEventListener('DOMContentLoaded', e => {
+  const shablon = template.innerHTML.trim();
+  console.log(shablon);
+  const compileTemplate = Handlebars.compile(shablon);
 
-document.body.innerHTML = hello('Artem');
-document.body.innerHTML = goodbye('Artem');
+  const a = compileTemplate({
+    userID: 2,
+    postId: 3,
+    postTitle:
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    postBody:
+      'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  });
 
-const audi = new Car();
+  const b = compileTemplate({
+    userID: 1,
+    postId: 2,
+    postTitle: 'a',
+    postBody: 'b',
+  });
+  // innerHTML - повністю очищує елемент після чого додає контент який ви попросили
+  document.body.innerHTML = a;
+  document.body.innerHTML = b;
+  document.body.insertAdjacentHTML('beforeend', a);
+});

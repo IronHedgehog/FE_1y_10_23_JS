efd3faf1f95fea8a00d9d91360f20f500701f8dd
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-01-26 11:59:16 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+let start = new Date();
+
 // new Date() - поточна дата та час на вашому місцізнаходження
 const constDate = new Date(Date.now());
 
+const now = Date.now();
+
+console.log(now);
 // .now() - повертає час з 70го року до зараз у мілісекундах
 // .getDate() - повертає значення з 1 до 31
 const date = constDate.getDate();
@@ -57,8 +62,56 @@ const options = {
 
 const locale = navigator.language;
 console.log(locale);
-const localeUk = dateDate.toLocaleString(locale, options);
+const localeUk = dateDate.toLocaleString(locale, {});
 console.log(localeUk);
 
 // Перетворення дати до числа
+
+let newDate = new Date();
+// Коли ми переводимо дату у число то ми отримуєм у UNIX часі поточну дату
+console.log(Number(newDate));
+// + - неявне приведення до числа
+console.log(+newDate);
+
+const timee = newDate.getTime();
+console.log(timee);
+// Початок відліку
+
 // Обчислення різниці між датами
+
+// for (let i = 0; i < 100000; i++) {
+//   console.log(i);
+// }
+// Кінець відліку
+let end = new Date();
+
+console.log(start);
+console.log(end);
+
+console.log(`Цей важкий цикл зайняв ${end - start}ms`);
+
+// let newDateMarch = new Date('March 8,2025');
+let newDateMarch = new Date('2025-03-08T08:00:00');
+
+console.log(newDateMarch);
+
+const dateEight = Number(newDateMarch) - Number(newDate);
+
+let marchHours = newDateMarch.getHours();
+let marchMinutes = newDateMarch.getMinutes();
+let marchSeconds = newDateMarch.getSeconds();
+let marchMS = newDateMarch.getMilliseconds();
+
+console.log(marchHours, marchMinutes, marchSeconds, marchMS);
+// console.log(marchTime);
+
+console.log(dateEight);
+
+let newTime = new Date(dateEight);
+
+console.log(newTime);
+
+// YYYY(year)-MM(month)-DD(day)T(роздільник)HH(hours):mm(minutes)-ss(seconds).
+let newTimee = Date.parse('2025-02-01T20:10:50');
+
+console.log(newTimee);

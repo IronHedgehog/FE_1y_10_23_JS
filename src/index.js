27619fa72fcd8580f682956ac49f926a6b5686f0
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-02-15 13:02:24 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,55 +1,11 @@
-const makePromise = (delay, text) => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      if (Math.floor(Math.random() * 10 + 1) < 5) {
-        res(text);
-      } else {
-        rej(text);
-      }
-    }, delay);
-  });
-};
+// fetch - інструмент для запитів на сервер який їде з коробки JS
 
-const promiseA = makePromise(1000, 'promiseA');
-const promiseB = makePromise(2000, 'promiseB');
-const promiseC = makePromise(3000, 'promiseC');
-const promiseD = makePromise(4000, 'promiseD');
-
-Promise.all([promiseA, promiseB, promiseC, promiseD])
-  .then(value => console.log(value))
-  .catch(error => {
-    console.log('Нас зареджектило');
-  })
-  .finally(() => {
-    console.log('PROMISE.ALL завершив виконання');
-  });
-
-Promise.race([promiseA, promiseB, promiseC, promiseD])
-  .then(value => console.log(value))
-  .catch(error => {
-    console.log('Нас зареджектило :', error);
-  })
-  .finally(() => {
-    console.log('PROMISE.RACE завершив виконання');
-  });
-
-Promise.any([promiseA, promiseB, promiseC, promiseD])
-  .then(value => console.log(value))
-  .catch(error => {
-    console.log('Нас зареджектило :', error.message);
-  })
-  .finally(() => {
-    console.log('PROMISE.ANY завершив виконання');
-  });
-
-new Promise(resolve => resolve('Успіх')).then(value => {
-  console.log(value);
-});
-
-Promise.resolve('Успіх').then(value => console.log(value));
-
-new Promise(rejected => rejected('Не успіх')).catch(value => {
-  console.log(value);
-});
-
-Promise.reject('Не успіх').catch(value => console.log(value));
+// fetch - приймає два параметри
+// Параметр 1-й: шлях за яким ви хочете отримати дані(ЗБЕРІГАЄТЬСЯ В ДОКУМЕНТАЦІЇ БЕКЕНДУ)
+// для отримання даних вам необхідний тільки один параметр(шлях)
+// МЕТОД FETCH - завжди повертає проміс
+fetch('https://jsonplaceholder.typicode.com/posts/10')
+  // Обробка даних
+  .then(response => response.json())
+  //дані
+  .then(json => console.log(json));
--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,12 +1,12 @@
-// 1) localStorage
-const LOCAL_STORAGE_INPUT_TEXT = 'input';
-const object = {
-  a: 'b',
-};
-// localStorage.getItem();
-localStorage.setItem(LOCAL_STORAGE_INPUT_TEXT, JSON.stringify(object));
+// // 1) localStorage
+// const LOCAL_STORAGE_INPUT_TEXT = 'input';
+// const object = {
+//   a: 'b',
+// };
+// // localStorage.getItem();
+// localStorage.setItem(LOCAL_STORAGE_INPUT_TEXT, JSON.stringify(object));
 
-console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_INPUT_TEXT)));
-console.log(localStorage.length);
-localStorage.removeItem(LOCAL_STORAGE_INPUT_TEXT);
-localStorage.clear();
+// console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_INPUT_TEXT)));
+// console.log(localStorage.length);
+// localStorage.removeItem(LOCAL_STORAGE_INPUT_TEXT);
+// localStorage.clear();

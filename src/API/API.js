774e287ccc2cfb687fb87ed51d3a647fb125e5a2
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-03-08 12:09:34 +0200

    a

--- src/API/API.js
+++ src/API/API.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
-export const getPosts = async (page = 1) => {
+export const getPosts = async (q = 'car', page = 1) => {
   try {
     const { data } = await axios(
-      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10&q=${q}`
     );
-
+    if (data.length === 0) {
+      throw new Error('Інформації немає');
+    }
     return data;
   } catch (error) {
-    throw new Error('Typical....');
+    throw new Error('За вашим запитом даних немає');
   }
 };
+
+// GET https://pixabay.com/api?key=""&q="car"&page=1&per_page=50
--- src/index.js
+++ src/index.js
@@ -1,34 +1,55 @@
 import { getPosts } from './API/API';
+import { load, save } from './js/storageScripts';
 
 const section = document.querySelector('#render');
 const LOAD_MORE_BUTTON = document.getElementById('load');
+const form = document.getElementById('searchForm');
+
+const Q_VALUE = 'qValue';
 
 let page = 1;
 
-async function makeHTML(page) {
-  const data = await getPosts(page);
+async function makeHTML(q, page) {
+  const data = await getPosts(q, page);
 
-  console.log(data);
-  const html =
-    data &&
-    data
-      .map(({ id, body, email, name }) => {
-        return `
+  const html = data
+    .map(({ id, body, email, name }) => {
+      return `
       <div id="${id}">
    <p>${body}</p>
    <p>${email}</p>
     <p>${name}</p>
-
     </div>
       `;
-      })
-      .join('');
-  section.innerHTML = html;
+    })
+    .join('');
+  section.insertAdjacentHTML('beforeend', html);
 }
 
-console.log(makeHTML());
+// document.addEventListener('DOMContentLoaded', async e => {
+//   await makeHTML();
+// });
 
-LOAD_MORE_BUTTON.addEventListener('click', async () => {
+LOAD_MORE_BUTTON.addEventListener('click', async e => {
   page++;
-  await makeHTML(page);
+  const value = load(Q_VALUE);
+  await makeHTML(value, page);
+});
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const qValue = e.currentTarget.elements.searchInput.value;
+
+  save(Q_VALUE, qValue);
+
+  section.innerHTML = '';
+  await makeHTML(qValue, 1);
+
+  form.reset();
+
+  page++;
+  if (page > 1) {
+    LOAD_MORE_BUTTON.style.display = 'block';
+  }
 });
